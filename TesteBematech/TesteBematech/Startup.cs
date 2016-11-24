90db94cbc762057841deb97f26c5442c809b1228
lgsscout <lgsscout@LGSSCOUTNEW>
2016-11-24 11:11:42 -0200

    Criação do Projeto

---
+++ TesteBematech/TesteBematech/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TesteBematech.Startup))]
+namespace TesteBematech
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
